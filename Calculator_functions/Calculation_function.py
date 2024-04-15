dc1bae245e3232f1cdcaada3324dbdaed582528b
RainiBenaiah <b.raini@alustudent.com>
2024-04-15 12:53:24 -0700

    edit

--- Calculator_functions/Calculation_function.py
+++ Calculator_functions/Calculation_function.py
@@ -1,21 +1,19 @@
-# introducing library
-# from codecarbon import EmissionsTracker
-# intialize it
-def transport_footprint(transport_method, num_of_ppl):
-# customize your function
+
+def transport_footprint(transport_method, distance):
+
   if transport_method == 'car':
-    transport_carbonft = num_of_ppl * 3.0
+    transport_carbonft = distance * 1.5
   elif transport_method == 'bus':
-    transport_carbonft == num_of_ppl * 1.2
+    transport_carbonft == distance * 1.0
   elif transport_method == 'lorry':
-    transport_carbonft = num_of_ppl * 0.5
+    transport_carbonft = distance * 2.5
   else:
     transport_carbonft = 0
   
   return transport_carbonft
   
 def electricity_footprint(usage):
-# customize the function
+
   if usage is None:
     return 0
   else:
@@ -24,21 +22,20 @@ def electricity_footprint(usage):
 
   
 def food_footprint(category, num_of_meals):
-# customize the function
+
   if category == 'beef':
-    food_carbonft = num_of_meals * 5.0
+    food_carbonft = num_of_meals * 2.5
   elif category == 'pork':
-    food_carbonft = num_of_meals * 3.0
+    food_carbonft = num_of_meals * 1.8
   elif category == 'chicken':
-    food_carbonft = num_of_meals * 2.0
+    food_carbonft = num_of_meals * 1.2
   else:
     food_carbonft = num_of_meals * 1.0
 
   return food_carbonft
 
-# calculate the total individual carbon footprint
-def calculate_total_carbon_footprint(transport_method, num_of_ppl, usage, category, num_of_meals):
-  individual_transportation_carbon = transport_footprint(transport_method, num_of_ppl)
+def calculate_total_carbon_footprint(transport_method, distance, usage, category, num_of_meals):
+  individual_transportation_carbon = transport_footprint(transport_method, distance)
   individual_electricity_carbon = electricity_footprint(usage)
   individual_food_carbon = food_footprint(category, num_of_meals)
 
@@ -47,7 +44,7 @@ def calculate_total_carbon_footprint(transport_method, num_of_ppl, usage, catego
 
 # inputs
 transport_method = input('enter your transport method:')
-num_of_ppl = float(input('enter the number of people:'))
+distance = float(input('enter the distance covered:'))
 usage = float(input('enter the electric usage:'))
 category = input('enter your food category:')
 num_of_meals = float(input('enter the num of meals:'))
@@ -56,7 +53,7 @@ num_of_meals = float(input('enter the num of meals:'))
 
 
 
-total_individual_carbon_footprint = calculate_total_carbon_footprint(transport_method, num_of_ppl, usage, category, num_of_meals)
+total_individual_carbon_footprint = calculate_total_carbon_footprint(transport_method, distance, usage, category, num_of_meals)
 
 print("Total Individual Carbon Footprint: {} tons CO2e".format(total_individual_carbon_footprint))
 
